4b3c39a914bda93613422052760946ad9091a842
Devin Mork <devin@finalsigma.io>
2023-09-06 18:34:05 -0500

    Complete lessons 19 and 20, finishing section 2.

--- Database.php
+++ Database.php
@@ -3,21 +3,26 @@
 class Database {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+        $dsn = "mysql:" . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn, 'root');
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
-    public function query($query)
+    public function query($query, $params = [])
     {
         $statement = $this->connection->prepare($query);
 
-        $statement->execute();
+        $statement->execute($params);
 
         return $statement;
     }
 }
+
+// Two colons: Scope resolution operator. ::
+// Provides access to static or constant that's defined on a class.
 
 // connect to MySQL database. PDO = PHP Data Objects
 // don't write this code that often because you're usually
--- index.php
+++ index.php
@@ -8,11 +8,15 @@ require "functions.php";
 require "Database.php";
 
 // Connect to the database, and execute a query.
+$config = require('config.php');
 
+$db = new Database($config['database']);
 
-$db = new Database();
+// never accept user input into a SQL query string.
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
 
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
 
