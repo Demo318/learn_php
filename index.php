e3ba0530b263fcd30c07e8d45f65b0acdfe194a6
Devin Mork <devin@finalsigma.io>
2023-09-06 13:31:02 -0500

    Completed follow-along for lesson 17

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+// connect to MySQL database. PDO = PHP Data Objects
+// don't write this code that often because you're usually
+// using a library.
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
