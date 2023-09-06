4f75e956933165ec39a300164b2812ab8e46933c
Devin Mork <devin@finalsigma.io>
2023-09-06 13:36:26 -0500

    Complete homework for lesson 17

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
 
 $pdo = new PDO($dsn, 'root');
 
-$statement = $pdo->prepare("select * from posts");
+$statement = $pdo->prepare("select * from posts where id = 1");
 
 $statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $statement->fetch(PDO::FETCH_ASSOC);
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+echo '<li>' . $posts['title'] . '</li>';
+
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
