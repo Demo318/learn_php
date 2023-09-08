15183d2879fea13a38d3d3e6965d4c0a9e9a8e78
Devin Mork <devin.mork@gmail.com>
2023-09-08 15:40:04 -0500

    Complete lesson 23

--- controllers/note.php
+++ controllers/note.php
@@ -4,9 +4,18 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'My Notes';
+$currentUserId = 3;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [ 
+    'id' => $_GET['id']
+])->fetch();
 
+if (! $note) {
+    abort();
+}
 
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
