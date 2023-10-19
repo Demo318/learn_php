8412186d655866c1591c21254dbed5714f349400
Devin Mork <devin.mork@gmail.com>
2023-10-19 14:47:35 -0500

    Complete lesson 33

--- routes.php
+++ routes.php
@@ -1,14 +1,15 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/note' => 'controllers/notes/show.php',
-    '/contact' => 'controllers/contact.php',
-    '/mission' => 'controllers/mission.php',
-];
+$router->get('/', 'controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
+$router->get('/mission', 'controllers/mission.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+$router->get('/notes/create', 'controllers/notes/create.php');
+
+// $router->delete('/note', 'controllers/notes/destroy.php');
 
 /*
 
